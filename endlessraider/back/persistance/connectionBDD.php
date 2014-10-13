609ec6fa6448a8d6928b4241277f5b3bb6ff0171
thorkh <thorkh.arokh@gmail.com>
2014-10-13 11:12:08 +0200

    Correction message erreur

--- endlessraider/back/persistance/connectionBDD.php
+++ endlessraider/back/persistance/connectionBDD.php
@@ -9,7 +9,7 @@
             return $connexion;
 	
         $PARAM_hote='localhost'; // le chemin vers le serveur
-		$PARAM_port='3306';
+		$PARAM_port='3306'; 
 		$PARAM_nom_bd='endlessraider'; // le nom de votre base de données
 		$PARAM_utilisateur='admin'; // nom d'utilisateur pour se connecter
 		$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
@@ -23,7 +23,7 @@
 		catch(Exception $e) {
 			//echo 'Erreur : '.$e->getMessage().'<br />';
 			//echo 'N° : '.$e->getCode();
-			throw new Exception('Erreur de connexion à la base de données');
+			throw new Exception('Erreur de connexion à la base de données'.$e->getMessage());
 		}
     }
    
