7960b410bf2c32a0b30482b2820acee242bc3571
Thorkh <Thorkh@Thorkh-PC>
2014-10-17 20:34:03 +0200

    Correction CSS

--- endlessraider/back/persistance/connectionBDD.php
+++ endlessraider/back/persistance/connectionBDD.php
@@ -9,10 +9,10 @@
             return $connexion;
 	
         $PARAM_hote='localhost'; // le chemin vers le serveur
-		$PARAM_port='3306'; 
+		$PARAM_port='3306';
 		$PARAM_nom_bd='endlessraider'; // le nom de votre base de données
 		$PARAM_utilisateur='admin'; // nom d'utilisateur pour se connecter
-		$PARAM_mot_passe=''; // mot de passe de l'utilisateur pour se connecter
+		$PARAM_mot_passe='admin'; // mot de passe de l'utilisateur pour se connecter
 		try {
 			$connexion = new PDO('mysql:host='.$PARAM_hote.';dbname='.$PARAM_nom_bd, 
 			$PARAM_utilisateur, $PARAM_mot_passe, 
@@ -23,7 +23,7 @@
 		catch(Exception $e) {
 			//echo 'Erreur : '.$e->getMessage().'<br />';
 			//echo 'N° : '.$e->getCode();
-			throw new Exception('Erreur de connexion à la base de données'.$e->getMessage());
+			throw new Exception('Erreur de connexion à la base de données');
 		}
     }
    
